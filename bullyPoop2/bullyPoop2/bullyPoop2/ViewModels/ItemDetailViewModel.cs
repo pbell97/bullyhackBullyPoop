2a12a6d2c0d9292b3b169737aa99fa6c363eaa43
claytonsewell <sewell316@gmail.com>
2020-01-11 14:44:04 -0600

    New Solution

---
+++ bullyPoop2/bullyPoop2/bullyPoop2/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using bullyPoop2.Models;
+
+namespace bullyPoop2.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
