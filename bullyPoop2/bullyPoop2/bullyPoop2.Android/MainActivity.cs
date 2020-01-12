e47e8ff1224f03db4b9cce831293ae9bf58b19bd
pbell97 <patrickb97@hotmail.com>
2020-01-11 19:19:21 -0600

    Added 'Add Bathroom' page

--- bullyPoop2/bullyPoop2/bullyPoop2.Android/MainActivity.cs
+++ bullyPoop2/bullyPoop2/bullyPoop2.Android/MainActivity.cs
@@ -16,12 +16,13 @@ namespace bullyPoop2.Droid
     [Activity(Label = "bullyPoop", Icon = "@mipmap/bp_icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+        List<Bathroom> allBathrooms;
+        List<string> buildings;
+
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
-            TabLayoutResource = Resource.Layout.Tabbar;
-            ToolbarResource = Resource.Layout.Toolbar;
-
+            allBathrooms = new List<Bathroom>();
             base.OnCreate(savedInstanceState);
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
@@ -38,9 +39,6 @@ namespace bullyPoop2.Droid
             };
 
             HomePage();
-
-
-
 
 
         }
@@ -62,24 +60,57 @@ namespace bullyPoop2.Droid
             {
                 //Change Content View
                 Console.WriteLine("Clicked Add Bathroom");
+                registerBathroom();
             };
 
-            var bathrooms = new List<Bathroom>();
             var bathroomNames = new List<String>();
 
-            bathrooms.Add(new Bathroom("Union", 1, 1, true, 4, 4));
-            bathrooms.Add(new Bathroom("Union", 1, 2, true, 8, 8));
-
-
-            for (var i = 0; i < bathrooms.Count; i++)
+            for (var i = 0; i < allBathrooms.Count; i++)
             {
-                var name = bathrooms[i].building + " - Floor " + bathrooms[i].floor;
-                if (bathrooms[i].number > 1) name += " - #" + bathrooms[i].number;
+                var name = allBathrooms[i].building + " - Floor " + allBathrooms[i].floor;
+                if (allBathrooms[i].number > 1) name += " - #" + allBathrooms[i].number;
                 bathroomNames.Add(name);
             }
 
             ArrayAdapter<string> adapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1, bathroomNames);
             bathroomListView.Adapter = adapter;
         }
+
+
+        public void registerBathroom()
+        {
+            SetContentView(Resource.Layout.registerBathroom);
+            var registerButton = FindViewById<Button>(Resource.Id.buttonRegisterBathroom);
+
+            registerButton.Click += (sender, e) =>
+            {
+                var building = FindViewById<EditText>(Resource.Id.editTextRegisterBuilding).Text;
+                var floor = FindViewById<EditText>(Resource.Id.editTextRegisterFloor).Text;
+                var number = FindViewById<EditText>(Resource.Id.editTextRegisterNumber).Text;
+                var stallsNumber = FindViewById<EditText>(Resource.Id.editTextRegisterNumberStalls).Text;
+                var urinalsNumber = FindViewById<EditText>(Resource.Id.editTextRegisterNumberUrinals).Text;
+                var handicap = FindViewById<CheckBox>(Resource.Id.checkBoxHandicap).Checked;
+
+                if (building == "" || floor == "" || number == "" || stallsNumber == "" || urinalsNumber == "")
+                {
+                    Console.WriteLine("A field is empty");
+                }
+                else
+                {
+                    Console.WriteLine("All fields are completed");
+                    var newBathroom = new Bathroom(building, Int32.Parse(floor), Int32.Parse(number), handicap, Int32.Parse(stallsNumber), Int32.Parse(urinalsNumber));
+                    allBathrooms.Add(newBathroom);
+
+                    // TODO On successful submit...
+                    HomePage();
+                }
+            };
+        }
+
+        public void addReview()
+        {
+            SetContentView(Resource.Layout.addReview);
+        }
+
     }
 }
