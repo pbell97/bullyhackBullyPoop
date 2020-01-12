961b9cc7619b5b8ca619c6c9e6a074ccbda942b6
anorris772 <43426457+anorris772@users.noreply.github.com>
2020-01-11 20:11:39 -0600

    Login page

---
+++ bullyPoop2/bullyPoop2/bullyPoop2.Android/Resources/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+using Xamarin.Essentials;
+
+namespace bullyPoop2.Droid.Resources
+{
+    public class MapTest
+    {
+        public async Task NavigateToBuilding25()
+        {
+            var location = new Location(47.645160, -122.1306032);
+            var options = new MapLaunchOptions { Name = "Microsoft Building 25" };
+
+            await Map.OpenAsync(location, options);
+        }
+    }
+}
